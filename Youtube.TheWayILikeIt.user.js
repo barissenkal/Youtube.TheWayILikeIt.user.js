7dc7b1b407f553d0d1f74afbffa84c5b7b734024
Baris Senkal <barissenkal@gmail.com>
2016-08-01 11:26:03 +0300

    changed style to better scale when window is small.

--- Youtube.TheWayILikeIt.user.js
+++ Youtube.TheWayILikeIt.user.js
@@ -3,14 +3,17 @@
 // @description  Sets YouTube's top bar position to relative. Makes the area around player #131313. Makes player size 1280px by 720px. Stops Autoplay. Adds clipconverter links to more menu.
 // @include      https://*.youtube.com/*
 // @include      http://*.youtube.com/*
-// @version      1.4.6
+// @version      1.4.7
 // @grant        none
 // @author       Baris Senkal etekmekketcap.com
 // ==/UserScript==
 
 var prepPlayer = function() {
 
-  if(document.webkitIsFullScreen) return;
+  if(document.webkitIsFullScreen){
+    removeCustomPlayerStyle();
+    return;
+  }
 
   var mcontainter = document.getElementById('yt-masthead-container'),
     theater = document.getElementById('theater-background'), //the theater background
@@ -18,9 +21,11 @@ var prepPlayer = function() {
     pplaylist = document.getElementById('watch-appbar-playlist');
     
   var style = document.getElementById("customstyle");
+  
   style.media = "";
     
   if(mcontainter && pplayer && theater){
+    
     pplayer.style.height = window.innerHeight +'px';
     theater.style.height = (window.innerHeight - 10) +'px';
 
@@ -28,14 +33,6 @@ var prepPlayer = function() {
 
   } else {
     console.log('Html of youtube changed?');
-  }
-
-
-  if(window.innerWidth > 1344){
-    
-    //Setting cookie to keep player wide
-    if(document.cookie) document.cookie="wide=1; path=/; domain=.youtube.com";
-
   }
 
 }
@@ -80,10 +77,13 @@ function start() {
     removeCustomPlayerStyle();
   },false);
 
-  //Detecting exit from fullscreen (Only works for webkit fullscreen)
-  document.addEventListener("webkitfullscreenchange", function (event) {
+  var timeout;
+  
+  //Detecting fullscreen as well?
+  window.addEventListener("resize", function (event) {
     if(!document.webkitIsFullScreen) {
-      setTimeout(function () {
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
         prepPlayer();
       }, 1000); // Timeout with an arbitary number is bad solution but it works.
     } else {
@@ -97,10 +97,13 @@ function start() {
 //For the case If video 'canplaythrough' before script runs
 (function(){
   
+  //Setting cookie to keep player wide
+  if(document.cookie) document.cookie="wide=1; path=/; domain=.youtube.com";
+  
   var style = document.createElement('style');
   style.id = 'customstyle';
   style.type = 'text/css';
-  style.innerHTML = '#masthead-positioner{position:relative;background:#131313}#masthead-positioner-height-offset{height:0!important}#theater-background{background:#131313!important}#yt-masthead-container{background:transparent;opacity:.3;border-bottom:1px solid #131313}@media screen and (min-width: 1344px){#player-api{width:1280px;height:720px;margin-left:488px}#content{overflow:hidden}.video-stream{width:1280px!important;height:720px!important;left:0!important}.ytp-chrome-bottom{left:225px}.ytp-size-button{display:none !important}}';
+  style.innerHTML = '#masthead-positioner{position:relative;background:#131313}#masthead-positioner-height-offset{height:0!important}#theater-background{background:#131313!important}#yt-masthead-container{background:transparent;opacity:.3;border-bottom:1px solid #131313}@media screen and (max-width: 1000px){#movie_player{position:fixed!important;top:0;right:0;bottom:0;left:0}#masthead-positioner{position:fixed}}@media screen and (min-width: 1344px){#player-api{width:1280px;height:720px;margin-left:488px}#content{overflow:hidden}.video-stream{width:1280px!important;height:720px!important;left:0!important}.ytp-chrome-bottom{left:225px}.ytp-size-button{display:none!important}}';
   console.log(document.getElementsByTagName('head')[0]);
   document.getElementsByTagName('head')[0].appendChild(style);
   
